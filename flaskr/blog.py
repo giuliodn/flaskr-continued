88b685d631201ec459c7d05840de216055b7d55b
Giulio <giuliodn@gmail.com>
2021-04-10 12:42:01 +0200

    part 3 completed

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -94,6 +94,25 @@ def get_comments(post_id, comment_id=None):
 
     return comments
 
+def get_tags(post_id):
+    """Get tags of a post.
+
+    :param post_id: id of post to get tags for
+    :return: the tags of post_id
+    :return: None if there're no tags
+    """
+    tags = (
+        get_db()
+        .execute(
+            "SELECT tags.id, tags.tag"
+            " FROM tags JOIN tagsofposts ON tags.id = tagsofposts.tag_id"
+            " WHERE post_id = ?",
+            (post_id, ),
+        )
+        .fetchall()
+    )
+
+    return tags
 
 
 @bp.route("/create", methods=("GET", "POST"))
@@ -103,20 +122,58 @@ def create():
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
+        tags = request.form["tags"]
         error = None
 
         if not title:
             error = "Title is required."
 
+        # Parse tags
+        # NOTE: this is a remainder for a more convenient parsing
+        forbidden_chars = set('#[]()`?$%/\<>*')
+        forbidden_chars_nicetxt = ' '.join(list(forbidden_chars))
+        if any((c in forbidden_chars) for c in tags):
+            error = "This characters %s are not allowed in tags, please remove them" % forbidden_chars_nicetxt
+
         if error is not None:
             flash(error)
         else:
             db = get_db()
-            db.execute(
+            # lastrowid gives the id of the newly inserted post 
+            # which is needed to associate tags
+            post_id= db.execute(
                 "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
                 (title, body, g.user["id"]),
-            )
+            ).lastrowid
             db.commit()
+
+            # turn tags to list and strip whitespaces
+            tags = tags.split(',')
+            tags = [x.strip() for x in tags]
+            print(tags)
+
+            for tag in tags:
+                # Check if tag is in database
+                tag_row = db.execute(
+                    "SELECT id FROM tags WHERE tag=?", (tag,),
+                ).fetchone()
+                if tag_row is None:
+                    # Tag not in the database, we need to insert it
+                    tag_id=db.execute(
+                        "INSERT INTO tags (tag) VALUES (?)",
+                        (tag,),
+                    ).lastrowid
+                    db.commit()
+                else:
+                    # If tag_row is not None then 'id' is inside it
+                    tag_id = tag_row['id']
+                # Now we can associate tag to post
+                db.execute(
+                    "INSERT INTO tagsofposts (tag_id, post_id) VALUES (?,?)",
+                    (tag_id, post_id),
+                )
+                db.commit()
+
             return redirect(url_for("blog.index"))
 
     return render_template("blog/create.html")
@@ -127,14 +184,28 @@ def create():
 def update(id):
     """Update a post if the current user is the author."""
     post = get_post(id)
+    tags_of_post = get_tags(id)
+
+    # Create a list of the tags of the post
+    tag_list = []
+    for tag in tags_of_post:
+        tag_list.append(tag['tag'])
 
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
+        tags = request.form["tags"]
         error = None
 
         if not title:
             error = "Title is required."
+
+        # Parse tags
+        # NOTE: this is a remainder for a more convenient parsing
+        forbidden_chars = set('#[]()`?$%/\<>*')
+        forbidden_chars_nicetxt = ' '.join(list(forbidden_chars))
+        if any((c in forbidden_chars) for c in tags):
+            error = "This characters %s are not allowed in tags, please remove them" % forbidden_chars_nicetxt
 
         if error is not None:
             flash(error)
@@ -144,9 +215,57 @@ def update(id):
                 "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
             )
             db.commit()
+
+            # turn tags to list and strip whitespaces
+            tags = tags.split(',')
+            tags = [x.strip() for x in tags]
+
+            for tag in tags:
+                # Check if tag is in database
+                tag_row = db.execute(
+                    "SELECT id FROM tags WHERE tag=?", (tag,),
+                ).fetchone()
+                if tag_row is None:
+                    # Tag not in the database, we need to insert it
+                    tag_id=db.execute(
+                        "INSERT INTO tags (tag) VALUES (?)",
+                        (tag,),
+                    ).lastrowid
+                    db.commit()
+                else:
+                    # If tag_row is not None then 'id' is inside it
+                    tag_id = tag_row['id']
+
+                # We must check if tag is already associated with post.
+                if tag not in tag_list:
+                    # tag not previously associated
+                    db.execute(
+                        "INSERT INTO tagsofposts (tag_id, post_id) VALUES (?,?)",
+                        (tag_id, id),
+                    )
+                    db.commit()
+                else:
+                    # remove tag from tag_list to check if user
+                    # deleted some tag 
+                    tag_list.remove(tag)
+
+            if len(tag_list) != 0:
+                # user removed some tags: break connection to post
+                for tag in tag_list:
+                    # first we need tag_id
+                    tag_id = db.execute("SELECT id FROM tags WHERE tag = ?", (tag, )).fetchone()
+                    tag_id = tag_id['id']
+                    db.commit()
+                    # then we break association
+                    db.execute("DELETE FROM tagsofposts WHERE post_id = ? AND tag_id = ?", (id, tag_id),)
+                    db.commit()
+
             return redirect(url_for("blog.index"))
 
-    return render_template("blog/update.html", post=post)
+    # Create comma separated string of all the tags
+    tags_string = ', '.join(tag_list)
+
+    return render_template("blog/update.html", post=post, tags=tags_string)
 
 
 @bp.route("/<int:id>/delete", methods=("POST",))
@@ -160,6 +279,7 @@ def delete(id):
     get_post(id)
     db = get_db()
     db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.execute("DELETE FROM tagsofposts WHERE post_id = ?", (id, ))
     db.commit()
     return redirect(url_for("blog.index"))
 
@@ -169,6 +289,7 @@ def show(id):
     """Show a post no matter who the author is"""
     post = get_post(id, check_author=False)
     comments = get_comments(id)
+    tags = get_tags(id)
 
     if request.method == "POST":
         # The form has a comment to save
@@ -193,7 +314,7 @@ def show(id):
         return redirect(url_for("blog.show", id=id))
 
     # The method is "GET": just show the page
-    return render_template("blog/show.html", post=post, comments=comments)
+    return render_template("blog/show.html", post=post, comments=comments, tags=tags)
 
 
 @bp.route("/<int:id>/show_like", endpoint='show_like')
@@ -339,3 +460,19 @@ def comment_delete(id):
     db.commit()
 
     return redirect(url_for("blog.show", id=id))
+
+
+@bp.route("/<int:tag_id>/show_tag", methods=("GET",))
+@login_required
+def show_tag(tag_id):
+    """Show all the posts associated with tag_id.
+    """
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username, likes, unlikes, nr_comments"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " JOIN tagsofposts t ON t.tag_id = ? AND t.post_id=p.id"
+        " ORDER BY created DESC",
+        (tag_id, )
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
